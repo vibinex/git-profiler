c119107ba3000285da465a14ed5516afa941b0a3
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2020-09-12 17:34:39 +0530

    ui: added material-ui/core and a button on home page

---
+++ client/src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Avatar, Button, Container, withStyles } from '@material-ui/core';
+import githubIcon from '../images/github-white.png';
+
+const styles = (theme) => ({
+	root: {
+		marginTop: theme.spacing(8),
+		justifyContent: "center",
+	},
+	signInButton: {
+		color: 'white',
+		background: 'black'
+	}
+})
+
+class Home extends React.Component {
+	render() {
+		const { classes } = this.props;
+		return (
+			<Container className={ classes.root }>
+				<Button
+					variant="contained"
+					className={ classes.signInButton }
+					startIcon={ <Avatar src={ githubIcon } /> }
+				>
+					Sign In with GitHub
+				</Button>
+			</Container>
+		)
+	}
+}
+
+export default withStyles(styles)(Home);
