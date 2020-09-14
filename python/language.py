d5b005341e9f4d37c9fa1c540e32a0e2046e1b9d
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2020-09-14 12:33:05 +0530

    converted language extractor to class so that we don't load language map multiple times

--- python/language.py
+++ python/language.py
@@ -1,43 +1,47 @@
 from ruamel.yaml import YAML
 import os
 
-def get_language_map():
-	yaml=YAML(typ='safe')
-	with open("../languages.yml") as f:
-		lang_map = f.read()
-	lang_map = yaml.load(lang_map)
-	return lang_map
+class LanguageExtractor():
+	def __init__(self):
+		self.lang_map = self.get_language_map()
 
-def get_language_from_file(filename):
-	_, file_extension = os.path.splitext(filename)
-	lang_map = get_language_map()
-	fn_options = []
-	ext_options = []
-	for key in lang_map.keys():
-		if 'extensions' in lang_map[key].keys():
-			if file_extension in lang_map[key]['extensions']:
-				ext_options.append(key)
-		elif 'filenames' in lang_map[key].keys():
-			if filename in lang_map[key]['filenames']:
-				fn_options.append(key)
-		else:
-			# print(key, lang_map[key])
-			pass
+	def get_language_map(self):
+		yaml=YAML(typ='safe')
+		with open("../languages.yml") as f:
+			lang_map = f.read()
+		lang_map = yaml.load(lang_map)
+		return lang_map
 
-	if len(fn_options) == 1:
-		return fn_options[0]
-	elif len(fn_options) == 0:
-		if len(ext_options) == 1:
-			return ext_options[0]
-		elif len(ext_options) == 0:
-			return None
+	def get_language_from_file(self, filename):
+		_, file_extension = os.path.splitext(filename)
+		fn_options = []
+		ext_options = []
+		for key in self.lang_map.keys():
+			if 'extensions' in self.lang_map[key].keys():
+				if file_extension in self.lang_map[key]['extensions']:
+					ext_options.append(key)
+			elif 'filenames' in self.lang_map[key].keys():
+				if filename in self.lang_map[key]['filenames']:
+					fn_options.append(key)
+			else:
+				# print(key, self.lang_map[key])
+				pass
+
+		if len(fn_options) == 1:
+			return fn_options[0]
+		elif len(fn_options) == 0:
+			if len(ext_options) == 1:
+				return ext_options[0]
+			elif len(ext_options) == 0:
+				return None
+			else:
+				print("Warning: Multiple languages found for file '"+filename+"': ", ext_options)
+				return ext_options[0]
 		else:
-			print("Warning: Multiple languages found for file '"+filename+"': ", ext_options)
-			return ext_options[0]
-	else:
-		print("Warning: Multiple languages found for file '"+filename+"': ", fn_options)
-		return fn_options[0]
+			print("Warning: Multiple languages found for file '"+filename+"': ", fn_options)
+			return fn_options[0]
 
 if __name__ == '__main__':
-	print(get_language_from_file("test.py"))
-	print(get_language_from_file('sshconfig.snip'))
+	le = LanguageExtractor()
+	print(le.get_language_from_file("test.py"))
+	print(le.get_language_from_file('sshconfig.snip'))
